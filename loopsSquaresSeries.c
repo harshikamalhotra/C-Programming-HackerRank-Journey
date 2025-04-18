d7be64307ac81a7d7c75620f67c89f0bdf036202
harshika.malhotra <malhotra.harshikaa@gmail.com>
2025-04-18 21:09:25 +0530

    Update loopsSquaresSeries.c

--- loopsSquaresSeries.c
+++ loopsSquaresSeries.c
@@ -1,3 +1,6 @@
+// Loops_Squares Series
+//     Write a program to generate the series 1,
+//     4, 9, 16, 25, ... of N numbers, but skip the numbers that are divisible by 3.
 #include <stdio.h>
 
 int main()
