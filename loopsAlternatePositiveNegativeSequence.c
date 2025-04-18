1ea792b29c2c5b714a465168c583b7394aa56cae
harshika.malhotra <malhotra.harshikaa@gmail.com>
2025-04-18 21:08:31 +0530

    Update loopsAlternatePositiveNegativeSequence.c

--- loopsAlternatePositiveNegativeSequence.c
+++ loopsAlternatePositiveNegativeSequence.c
@@ -1,3 +1,5 @@
+// Loops_Alternate Positive/Negative Sequence
+// Write a program to generate the first n terms of a sequence where the numbers alternate between positive and negative integers, starting with 1.
 #include <stdio.h>
 
 int main()
